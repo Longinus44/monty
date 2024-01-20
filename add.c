b7d45c0c1a3d1f7e772ebf81faa3a0ac3ac37f03
chijioke <chijiokelonginus4@gmail.com>
2024-01-20 02:10:17 +0100

    added add function

--- add.c
+++ add.c
@@ -1,24 +1,20 @@
-#!/bin/bash
-
 #include "monty.h"
 
 /**
- * add - Adds the top two values of a stack_t linked list.
- * @stack: A pointer to the top mode node of a stack_t linked list.
- * @line_number: The current working line number of a Monty bytecodes file.
- *
- * Description: The result is stored in the second value node
- *              from the top and the top value  is removed.
+ * m_add - Adds the top two elements of the stack.
+ * @stack: Pointer to a pointer pointing to top node of the stack.
+ * @line_number: Interger representing the line number of of the opcode.
  */
-void add(stack_t **stack, unsigned int line_number)
+void m_add(stack_t **stack, unsigned int line_number)
 {
-    if ((*stack)->next == NULL || (*stack)->next->next == NULL)
-    {
-        set_op_tok_error(short_stack_error(line_number, "add"));
-        return;
-    }
+	int sum;
 
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_number, "add");
 
-    (*stack)->next->next->n += (*stack)->next->n;
-    monty_pop(stack, line_number);
+	(*stack) = (*stack)->next;
+	sum = (*stack)->n + (*stack)->prev->n;
+	(*stack)->n = sum;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
 }
